99b39dc557c2ec24c2d3f07c85cb092736a88220
Swetha Ganapathi Raman <swethag04@gmail.com>
2024-03-20 14:02:40 -0700

    add streamlit app

---
+++ llamaindex_doc_qna/llamaindex_helper.py
@@ -0,0 +1,54 @@
+import os
+from secretkey import openapi_key
+os.environ['OPENAI_API_KEY'] = openapi_key
+
+import openai
+api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = api_key
+# Print the API key for verification
+# print(f"API key: {api_key}")
+
+from llama_index.llms import OpenAI
+
+
+
+from llama_index import VectorStoreIndex, SimpleDirectoryReader, ServiceContext, OpenAIEmbedding
+from llama_index.llms import OpenAI
+from llama_index.llms import HuggingFaceInferenceAPI
+
+
+import nest_asyncio
+
+nest_asyncio.apply()
+
+def load_docs():
+    """
+    Load a pdf document that will provide additional context to LLM
+    """
+    docs = SimpleDirectoryReader(".").load_data()
+    return docs
+
+def get_vector_store(docs, zephyr_llm):
+    """
+        Split document into smaller sized chunks for embedding
+        and store document chunks in a vector store
+    """
+
+    index = VectorStoreIndex.from_documents(docs,
+                                        service_context=ServiceContext.from_defaults(chunk_size=512),
+                                        llm = zephyr_llm)
+    return index
+
+def build_query_engine(index):
+    query_engine = index.as_query_engine()
+    return query_engine
+
+def generate_answer(question):
+   
+    zephyr_llm = HuggingFaceInferenceAPI(model_name="HuggingFaceH4/zephyr-7b-alpha",
+                                        token="")
+    docs = load_docs()
+    index = get_vector_store(docs, zephyr_llm)
+    qe = build_query_engine(index)
+    response = qe.query(question)
+    return response
---
+++ llamaindex_doc_qna/main.py
@@ -0,0 +1,18 @@
+import os
+import streamlit as st
+import llamaindex_helper
+from tempfile import NamedTemporaryFile
+
+
+st.title("Q&A App for your PDF document")
+st.subheader('Upload your PDF file')
+input_file = st.file_uploader('⬆️ Upload your PDF & Click to process',
+                                 accept_multiple_files = False, 
+                                 type=['pdf'])
+if st.button("Process"):
+    with NamedTemporaryFile(dir='.', suffix='.pdf') as f:
+        f.write(input_file.getbuffer())
+user_question = st.text_input("Ask a question from the doc")
+if st.button("Submit"):
+    ans = llamaindex_helper.generate_answer(user_question)
+    st.write(ans.response)
